e05905b77fcc450836b9f69e446d824cac61d97d
vkraev <vladimir.kraev@gmail.com>
2021-07-03 13:07:30 +0700

    Initial Commit

---
+++ AgreeOfAnything/AgreeOfAnythingApp.swift
@@ -0,0 +1,17 @@
+//
+//  AgreeOfAnythingApp.swift
+//  AgreeOfAnything
+//
+//  Created by Vladimir A. Kraev on 03.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct AgreeOfAnythingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
